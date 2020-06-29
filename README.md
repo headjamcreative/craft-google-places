9fd7ce6c6588cb2ee5679b87fcc59d3f0e50dcb1
Ben Norman <ben.r.norman@gmail.com>
2020-06-30 09:20:43 +1000

    Initial pluginfactory.io scaffolding

---
+++ README.md
@@ -0,0 +1,7 @@
+# craft-google-places plugin for Craft CMS 3.x
+
+Sycns Google Places API data to entries.
+
+## Requirements
+
+This plugin requires Craft CMS 3.0.0-beta.23 or later.
---
+++ src/translations/en/craft-google-places.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * craft-google-places plugin for Craft CMS 3.x
+ *
+ * Sycns Google Places API data to entries.
+ *
+ * @link      https://www.headjam.com.au
+ * @copyright Copyright (c) 2020 Ben Norman
+ */
+
+/**
+ * craft-google-places en Translation
+ *
+ * Returns an array with the string to be translated (as passed to `Craft::t('craft-google-places', '...')`) as
+ * the key, and the translation as the value.
+ *
+ * http://www.yiiframework.com/doc-2.0/guide-tutorial-i18n.html
+ *
+ * @author    Ben Norman
+ * @package   CraftGooglePlaces
+ * @since     1.0.0
+ */
+return [
+    'craft-google-places plugin loaded' => 'craft-google-places plugin loaded',
+];
